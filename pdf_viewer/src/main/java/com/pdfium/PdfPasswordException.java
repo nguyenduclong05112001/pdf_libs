2d0eb3685555522ce154d5563f4491659b66b01f
NGUYEN DUC LONG <long.nd@intelin.vn>
2024-08-08 17:02:41 +0700

    init projet

---
+++ pdf_viewer/src/main/java/com/pdfium/PdfPasswordException.java
@@ -0,0 +1,13 @@
+package com.pdfium;
+
+import java.io.IOException;
+
+public class PdfPasswordException extends IOException {
+    public PdfPasswordException() {
+        super();
+    }
+
+    public PdfPasswordException(String detailMessage) {
+        super(detailMessage);
+    }
+}
